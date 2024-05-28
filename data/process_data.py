7e0e3f27274dc59be648acc3960c9affa4dfed33
Dan Herman <dan.herman@me.com>
2024-05-28 13:17:30 -0400

    updated files

--- data/process_data.py
+++ data/process_data.py
@@ -1,21 +1,44 @@
 import sys
 import pandas as pd
 
+
 # create function to load data, take filepath as input from text data and labels
 def load_data(messages_filepath, categories_filepath):
+    """
+    This function loads the data from the messages and categories files and merges them into a single dataframe.
+
+    Args:
+    messages_filepath: string. Filepath for messages csv file.
+    categories_filepath: string. Filepath for categories csv file.
+
+    Returns:
+    df: pandas DataFrame. Dataframe containing messages and categories merged.
+
+    """
     # load messages
     messages = pd.read_csv(messages_filepath)
     # load categories
     categories = pd.read_csv(categories_filepath)
     # merge datasets
-    df = pd.merge(messages, categories, on='id', how='inner')
+    df = pd.merge(messages, categories, on="id", how="inner")
 
     return df
 
+
 # create function to clean the data, prepared for machine learning
 def clean_data(df):
+    """
+    This function cleans the data by splitting the categories column into separate, clearly named columns, converting values to binary, and removing duplicates.
+
+    Args:
+    df: pandas DataFrame. Dataframe containing messages and categories merged.
+
+    Returns:
+    df: pandas DataFrame. Dataframe containing messages and categories merged, cleaned.
+
+    """
     # create a dataframe of the 36 individual category columns
-    categories = df['categories'].str.split(';', expand=True)
+    categories = df["categories"].str.split(";", expand=True)
     # select the first row of the categories dataframe
     row = categories.iloc[0]
     # use this row to extract a list of new column names for categories
@@ -29,54 +52,77 @@ def clean_data(df):
         # convert column from string to numeric
         categories[column] = categories[column].astype(int)
     # drop the original categories column from `df`
-    df.drop('categories', axis=1, inplace=True)
+    df.drop("categories", axis=1, inplace=True)
+    # drop rows that contain a value of 2 in the `related` column
+    df = df[df["related"] != 2]
     # concatenate the original dataframe with the new `categories` dataframe
     df = pd.concat([df, categories], axis=1)
     # drop duplicates
     df.drop_duplicates(inplace=True)
 
     return df
-    
+
 
 # create function to save the data, take the cleaned dataset and save it to an sqlite database
 def save_data(df, database_filename):
+    """
+    This function saves the clean dataset into an sqlite database.
+
+    Args:
+    df: pandas DataFrame. Dataframe containing messages and categories merged, cleaned.
+    database_filename: string. Filepath for database to save the cleaned data to.
+
+
+    """
     # save the clean dataset into an sqlite database
     from sqlalchemy import create_engine
-    engine = create_engine('sqlite:///' + 'disaster_response.db')
-    
-    return df.to_sql('features', engine, index=False, if_exists='replace')
+
+    engine = create_engine("sqlite:///" + database_filename)
+
+    return df.to_sql("features", engine, index=False, if_exists="replace")
+
 
 # create main function to run the ETL pipeline
 def main():
+    """
+    This function runs the ETL pipeline to load, clean, and save the data.
+
+    """
     # check for correct number of arguments
     if len(sys.argv) == 4:
         # assign arguments to variables
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
         # load data
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        print(
+            "Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}".format(
+                messages_filepath, categories_filepath
+            )
+        )
         df = load_data(messages_filepath, categories_filepath)
 
         # clean data
-        print('Cleaning data...')
+        print("Cleaning data...")
         df = clean_data(df)
-        
-        # save data
-        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
-        save_data(df, database_filepath)
-        
-        # print message to confirm data saved
-        print('Cleaned data saved to database!')
-    
-    else: # print message to provide correct arguments
-        print('Please provide the filepaths of the messages and categories '\
-              'datasets as the first and second argument respectively, as '\
-              'well as the filepath of the database to save the cleaned data '\
-              'to as the third argument. \n\nExample: python process_data.py '\
-              'disaster_messages.csv disaster_categories.csv '\
-              'DisasterResponse.db')
 
-# 
-if __name__ == '__main__':
+        # save data
+        print("Saving data...\n    DATABASE: {}".format(database_filepath))
+        save_data(df, database_filepath)
+
+        # print message to confirm data saved
+        print("Cleaned data saved to database!")
+
+    else:  # print message to provide correct arguments
+        print(
+            "Please provide the filepaths of the messages and categories "
+            "datasets as the first and second argument respectively, as "
+            "well as the filepath of the database to save the cleaned data "
+            "to as the third argument. \n\nExample: python process_data.py "
+            "disaster_messages.csv disaster_categories.csv "
+            "DisasterResponse.db"
+        )
+
+
+#
+if __name__ == "__main__":
     main()
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,40 +1,80 @@
 # import libraries
-import re, sys # regular expression and system libraries
-import numpy as np # numerical python 
-import pandas as pd # data manipulation
-from nltk.corpus import stopwords # natural language toolkit
-from nltk.tokenize import word_tokenize # natural language toolkit
-from nltk.stem import WordNetLemmatizer # natural language toolkit
-from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer # scikit-learn, text processing, feature extraction
-from sklearn.model_selection import train_test_split # scikit-learn, model selection, splitting data
-from sklearn.multioutput import MultiOutputClassifier # scikit-learn, multioutput classifier, 
-from sklearn.ensemble import RandomForestClassifier # scikit-learn, ensemble, random forest classifier
-from sklearn.pipeline import Pipeline # scikit-learn, pipeline object for chaining multiple estimators
-from sklearn.metrics import classification_report, accuracy_score, multilabel_confusion_matrix, f1_score # model evaluation objects
-from sqlalchemy import create_engine # SQL toolkit and Object Relational Mapper
-import joblib # joblib is a set of tools to provide lightweight pipelining in Python
-import warnings # warning control
-from sklearn.model_selection import GridSearchCV # scikit-learn, model selection, grid search
+import re, sys  # regular expression and system libraries
+import numpy as np  # numerical python
+import pandas as pd  # data manipulation
+from nltk.corpus import stopwords  # natural language toolkit
+from nltk.tokenize import word_tokenize  # natural language toolkit
+from nltk.stem import WordNetLemmatizer  # natural language toolkit
+from sklearn.feature_extraction.text import (
+    CountVectorizer,
+    TfidfTransformer,
+)  # scikit-learn, text processing, feature extraction
+from sklearn.model_selection import (
+    train_test_split,
+)  # scikit-learn, model selection, splitting data
+from sklearn.multioutput import (
+    MultiOutputClassifier,
+)  # scikit-learn, multioutput classifier,
+from sklearn.ensemble import (
+    RandomForestClassifier,
+)  # scikit-learn, ensemble, random forest classifier
+from sklearn.pipeline import (
+    Pipeline,
+)  # scikit-learn, pipeline object for chaining multiple estimators
+from sklearn.metrics import (
+    classification_report,
+    accuracy_score,
+    multilabel_confusion_matrix,
+    f1_score,
+)  # model evaluation objects
+from sqlalchemy import create_engine  # SQL toolkit and Object Relational Mapper
+import joblib  # joblib is a set of tools to provide lightweight pipelining in Python
+import warnings  # warning control
+from sklearn.model_selection import (
+    GridSearchCV,
+)  # scikit-learn, model selection, grid search
 
-warnings.filterwarnings("ignore") # ignore warnings
+warnings.filterwarnings("ignore")  # ignore warnings
+
 
 # create function to load data from SQLite database
 # setup variables for machine learning
 def load_data(database_filepath):
+    """
+    This function loads data from SQLite database and assigns variables for machine learning.
+
+    Args:
+    database_filepath: string. Filepath for SQLite database containing cleaned data.
+
+    Returns:
+    X: pandas Series. Series containing messages.
+    Y: pandas DataFrame. DataFrame containing categories.
+    category_names: list. List of category names.
+    """
     # create object of SQLite engine
-    engine = create_engine('sqlite:///' + database_filepath)
+    engine = create_engine("sqlite:///" + database_filepath)
     # read data from SQLite database assign to df
-    df = pd.read_sql_table('features', engine)
+    df = pd.read_sql_table("features", engine)
     # assign X and Y values
-    X = df['message']
+    X = df["message"]
     Y = df.iloc[:, 4:]
     # assign category names, will serve as column names for transformation
     category_names = Y.columns
 
     return X, Y, category_names
 
+
 # create function to process the text
 def tokenize(text):
+    """
+    This function processes the text data by normalizing, tokenizing, removing stop words, and lemmatizing.
+
+    Args:
+    text: string. Text data to be processed.
+
+    Returns:
+    words: list. List of processed words.
+    """
     # Normalize text
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
     # Tokenize text
@@ -46,23 +86,29 @@ def tokenize(text):
 
     return words
 
+
 # create function to build the model and pipeline steps
 def build_model():
-    pipeline = Pipeline([ # Pipeline of transforms with a final estimator
-        ('vect', CountVectorizer(tokenizer=tokenize,
-                                 min_df=5,
-                                 max_df=0.75)), # Convert a collection of text documents to a matrix of token counts
-        ('tfidf', TfidfTransformer()), # Transform a count matrix to a normalized tf or tf-idf representation
-        ('clf', MultiOutputClassifier(RandomForestClassifier())) # Multi target classification
-    ])
+    """
+    This function builds a machine learning pipeline with a grid search object.
+
+    Returns:
+    cv: GridSearchCV object. Grid search object with pipeline and parameters.
+    """
+    pipeline = Pipeline([  
+            ( "vect", CountVectorizer(tokenizer=tokenize, min_df=5, max_df=0.75)),  # Convert a collection of text 
+            ( "tfidf", TfidfTransformer()),  # Transform a count matrix to a normalized tf or tf-idf representation
+            ("clf", MultiOutputClassifier(RandomForestClassifier())),  # Multi target classification
+        ]
+    )
 
     # specify parameters for grid search
     parameters = {
-        'vect__ngram_range': ((1, 1), (1, 2)), # The lower and upper boundary of the range of n-values for different n-grams to be extracted
-        'clf__estimator__n_estimators': [50, 100], # The number of trees in the forest
-        'clf__estimator__min_samples_split': [2, 4] # The minimum number of samples required to split an internal node
+        "vect__ngram_range": ((1, 1),(1, 2)),  
+        "clf__estimator__n_estimators": [50, 100],  
+        "clf__estimator__min_samples_split": [2,4],  # The minimum number of samples required to split an internal node
     }
-    
+
     # create grid search object
     cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2, n_jobs=1)
 
@@ -71,59 +117,93 @@ def build_model():
 
 # create function to evaluate the model results
 def evaluate_model(model, X_test, y_test, category_names):
+    """
+    This function evaluates the model by predicting on test data and printing classification report for each category.
+
+    Args:
+    model: GridSearchCV object. Trained model.
+    X_test: pandas Series. Series containing messages for testing.
+    y_test: pandas DataFrame. DataFrame containing categories for testing.
+    category_names: list. List of category names.
+
+    """
     # predict on test data
     y_pred = model.predict(X_test)
     # loop through each column and print classification report
     for i, col in enumerate(y_test.columns):
         # print classification report
-        print(f'label:',col)
+        print(f"label:", col)
         print(classification_report(y_test[col], y_pred[:, i]))
-        print('Accuracy: {:.2f}'.format(accuracy_score(y_test[col], y_pred[:, i])))
-        print('F1 Score: {:.2f}'.format(f1_score(y_test[col], y_pred[:, i], average='weighted')))
+        print("Accuracy: {:.2f}".format(accuracy_score(y_test[col], y_pred[:, i])))
+        print(
+            "F1 Score: {:.2f}".format(
+                f1_score(y_test[col], y_pred[:, i], average="weighted")
+            )
+        )
         print()
-        print('Confusion Matrix:\n ', multilabel_confusion_matrix(y_test[col], y_pred[:, i]))
-        print('------------------------------------------------------')
+        print(
+            "Confusion Matrix:\n ",
+            multilabel_confusion_matrix(y_test[col], y_pred[:, i]),
+        )
+        print("------------------------------------------------------")
+
 
 # create function to save the model
 def save_model(model, model_filepath):
+    """
+    This function saves the model to a pickle file.
+
+    Args:
+    model: GridSearchCV object. Trained model.
+    model_filepath: string. Filepath to save the model to.
+
+    """
     joblib.dump(model, model_filepath, compress=7)
+
 
 # create main function to run the script
 def main():
+    """
+    This function runs the machine learning pipeline to load, train, evaluate, and save the model.
+
+    """
     # check for correct number of arguments
     if len(sys.argv) == 3:
         # assign arguments to variables
         database_filepath, model_filepath = sys.argv[1:]
         # load data
-        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        print("Loading data...\n    DATABASE: {}".format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         # build model
-        print('Building model...')
+        print("Building model...")
         model = build_model()
-        
+
         # train model
-        print('Training model...')
+        print("Training model...")
         model.fit(X_train, Y_train)
-        
+
         # evaluate model
-        print('Evaluating model...')
+        print("Evaluating model...")
         evaluate_model(model, X_test, y_test, category_names)
 
         # save model
-        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        print("Saving model...\n    MODEL: {}".format(model_filepath))
         save_model(model, model_filepath)
 
         # print message to confirm model saved
-        print('Trained model saved!')
-    
-    else: # print message to provide correct arguments 
-        print('Please provide the filepath of the disaster messages database '\
-              'as the first argument and the filepath of the pickle file to '\
-              'save the model to as the second argument. \n\nExample: python '\
-              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+        print("Trained model saved!")
+
+    else:  # print message to provide correct arguments
+        print(
+            "Please provide the filepath of the disaster messages database "
+            "as the first argument and the filepath of the pickle file to "
+            "save the model to as the second argument. \n\nExample: python "
+            "train_classifier.py ../data/DisasterResponse.db classifier.pkl"
+        )
+
 
 # run main function
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
